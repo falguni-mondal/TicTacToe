58e454f551e97d68ac3e5bbbbabdc445ead36aeb
Falguni Mondal <career.falguni@gmail.com>
2024-03-24 20:10:47 +0530

    Addition of base files

---
+++ index.js
@@ -0,0 +1,52 @@
+const box = document.querySelectorAll('.box');
+let count = document.querySelector('.container');
+let winTxt = document.querySelector('.msg h2');
+const win = document.querySelector('.win-msg');
+const winMsg = document.querySelector('.msg');
+for(let i=0; i<box.length;i++){
+    box[i].addEventListener('click', ()=>{
+        if(box[i].innerHTML === ''){
+            count.classList.toggle('x');
+            if(count.classList.contains('x')){
+                box[i].innerHTML = 'X';
+            }
+            else{
+                box[i].innerHTML = 'O';
+            }
+        }
+        if(box[0].innerHTML!='' && box[0].innerHTML === box[3].innerHTML && box[3].innerHTML === box[6].innerHTML){
+            winning(box[0].innerHTML);
+        }
+        else if(box[0].innerHTML!='' && box[0].innerHTML === box[4].innerHTML && box[4].innerHTML === box[8].innerHTML){
+            winning(box[0].innerHTML);
+        }
+        else if(box[0].innerHTML!='' && box[0].innerHTML === box[1].innerHTML && box[1].innerHTML === box[2].innerHTML){
+            winning(box[0].innerHTML);
+        }
+        else if(box[1].innerHTML!='' && box[1].innerHTML === box[4].innerHTML && box[4].innerHTML === box[7].innerHTML){
+            winning(box[1].innerHTML);
+        }
+        else if(box[2].innerHTML!='' && box[2].innerHTML === box[4].innerHTML && box[4].innerHTML === box[6].innerHTML){
+            winning(box[2].innerHTML);
+        }
+        else if(box[2].innerHTML!='' && box[2].innerHTML === box[5].innerHTML && box[5].innerHTML === box[8].innerHTML){
+            winning(box[2].innerHTML);
+        }
+        else if(box[3].innerHTML!='' && box[3].innerHTML === box[4].innerHTML && box[4].innerHTML === box[5].innerHTML){
+            winning(box[3].innerHTML);
+        }
+        else if(box[6].innerHTML!='' && box[6].innerHTML === box[7].innerHTML && box[7].innerHTML === box[8].innerHTML){
+            winning(box[6].innerHTML);
+        }
+    });
+}
+function winning(player){
+    winTxt.innerHTML = `🎉${player} Wins🎊`;
+    win.style.display = "flex";
+}
+win.querySelector('.rematch').addEventListener('click', ()=>{
+    box.forEach((val)=>{
+        val.innerHTML='';
+    })
+    win.style.display = "none";
+})
